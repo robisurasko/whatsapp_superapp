94b4032a43d7a1f962d0f12fea21e73150e4fd9b
Robi Tejo Surasko <robisurasko@gmail.com>
2025-11-06 09:37:18 +0700

    update installer py
    update installer dan manifest

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="whatsapp_superapp",
+    version="0.0.1",
+    description="Whatsapp Superapp for Customer Solution Integrated System",
+    author="Robi Tejo Surasko",
+    author_email="admin@robi.my.id",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
